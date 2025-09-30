b87e9c21e44ad59ffb1fd296135cd0ff49bff54d
RP272 <272931@student.pwr.edu.pl>
2025-09-30 10:26:04 +0200

    chore: add dependencies for PyTorch

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.pytorch.android.lite)
+    implementation(libs.pytorch.android.torchvision.lite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
