a18168c71ebe78d4963e9cb0cc13ed730ad4b3f5
Robert Pytel <272931@student.pwr.edu.pl>
2025-10-17 13:36:25 +0200

    feat: hide empty TextViews

--- mobile/app/src/main/java/com/example/mushroomclassifier/ui/about/LicenseAdapter.kt
+++ mobile/app/src/main/java/com/example/mushroomclassifier/ui/about/LicenseAdapter.kt
@@ -1,15 +1,25 @@
 package com.example.mushroomclassifier.ui.about
 
+import android.graphics.Color
+import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mushroomclassifier.R
 import com.example.mushroomclassifier.data.model.LicenseData
+import kotlin.reflect.KClass
 
 class LicenseAdapter(private val items: List<LicenseData>) :
     RecyclerView.Adapter<LicenseAdapter.LicenseViewHolder>() {
+
+    private val expandedStates = BooleanArray(items.size) { false }
+
     class LicenseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val resourceName: TextView = itemView.findViewById(R.id.resourceName)
         val resourceURL: TextView = itemView.findViewById(R.id.resourceURL)
@@ -21,6 +31,8 @@ class LicenseAdapter(private val items: List<LicenseData>) :
         val source3URL: TextView = itemView.findViewById(R.id.source3URL)
         val source4URL: TextView = itemView.findViewById(R.id.source4URL)
         val author: TextView = itemView.findViewById(R.id.author)
+        val licenseCardMain: CardView = itemView.findViewById(R.id.licenseCardMain)
+        val resourceInformation: LinearLayout = itemView.findViewById(R.id.resourceInformation)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LicenseViewHolder {
@@ -32,15 +44,78 @@ class LicenseAdapter(private val items: List<LicenseData>) :
     override fun onBindViewHolder(holder: LicenseViewHolder, position: Int) {
         val item = items[position]
 
-        holder.resourceName.text = item.resourceName
-        holder.resourceURL.text = "Resource URL: ${item.resourceURL}"
-        holder.resourceLicense.text = "Resource license: ${item.resourceLicense}"
-        holder.resourceSiteURL.text = "Resource site URL: ${item.resourceSiteURL}"
-        holder.source1URL.text = "Source 1: ${item.source1URL}"
-        holder.source2URL.text = "Source 2: ${item.source2URL}"
-        holder.source3URL.text = "Source 3: ${item.source3URL}"
-        holder.source4URL.text = "Source 4: ${item.source4URL}"
-        holder.author.text = "Author: ${item.author}"
+        if(item.resourceName.equals("Edibility icon")){
+            print("jajo")
+        }
+
+        if(!item.resourceName.isEmpty()){
+            holder.resourceName.text = item.resourceName
+            holder.resourceName.isVisible = true
+        }else{
+            holder.resourceName.isVisible = false
+        }
+
+        if(!item.resourceLicense?.isEmpty()!!){
+            holder.resourceLicense.text = "Resource license: ${item.resourceLicense}"
+            holder.resourceLicense.isVisible = true
+        }else{
+            holder.resourceLicense.isVisible = false
+        }
+
+        if(!item.resourceURL?.isEmpty()!!){
+            holder.resourceURL.text = "Resource URL: ${item.resourceURL}"
+            holder.resourceURL.isVisible = true
+        }else{
+            holder.resourceURL.isVisible = false
+        }
+
+        if(!item.resourceSiteURL.isNullOrEmpty()){
+            holder.resourceSiteURL.text = "Resource site URL: ${item.resourceSiteURL}"
+            holder.resourceSiteURL.isVisible = true
+        }else{
+            holder.resourceSiteURL.isVisible = false
+        }
+
+        if(!item.source1URL?.isEmpty()!!){
+            holder.source1URL.text = "Source 1: ${item.source1URL}"
+            holder.source1URL.isVisible = true
+        }else{
+            holder.source1URL.isVisible = false
+        }
+
+        if(!item.source2URL?.isEmpty()!!){
+            holder.source2URL.text = "Source 2: ${item.source2URL}"
+            holder.source2URL.isVisible = true
+        }else{
+            holder.source2URL.isVisible = false
+        }
+
+        if(!item.source3URL?.isEmpty()!!){
+            holder.source3URL.text = "Source 3: ${item.source3URL}"
+            holder.source3URL.isVisible = true
+        }else{
+            holder.source3URL.isVisible = false
+        }
+
+        if(!item.source4URL?.isEmpty()!!){
+            holder.source4URL.text = "Source 4: ${item.source4URL}"
+            holder.source4URL.isVisible = true
+        }else{
+            holder.source4URL.isVisible = false
+        }
+
+        if(!item.author?.isEmpty()!!){
+            holder.author.text = "Author: ${item.author}"
+            holder.author.isVisible = true
+        }else{
+            holder.author.isVisible = false
+        }
+
+        holder.resourceInformation.isVisible = expandedStates[position]
+        holder.licenseCardMain.setOnClickListener {
+            expandedStates[position] = !expandedStates[position]
+            notifyItemChanged(position)
+        }
     }
 
     override fun getItemCount() = items.size
